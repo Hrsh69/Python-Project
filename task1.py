196bc33da9de40104f0e8058fbc1ce5abe9dfe8a
Harsh <159132586+Hrsh69@users.noreply.github.com>
2025-06-09 18:28:31 +0530

    assignment_1

---
+++ task1.py
@@ -0,0 +1,13 @@
+# inputing the number and defining the character
+num1 = float(input('Enter first number: '))
+num2 = float(input('Enter second number: '))
+
+# operation
+addition = num1+num2
+substraction = num1-num2
+multiplication = num1*num2
+division = num1/num2
+print("addition: ",addition)
+print("substraction: ",substraction)
+print("multiplication: ",multiplication)
+print("division: ",division)
---
+++ task2.py
@@ -0,0 +1,5 @@
+first_name= str(input("Enter your first Name: "))
+last_name= str(input("Enter your last Name: "))
+full_name = first_name + " " + last_name
+welcome_message = ('Hello ,'+full_name + '! welcome to the python program')
+print (welcome_message)
